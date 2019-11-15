683023eacd97283acbe6aa63f9c8d0f7b5b4b88a
icanul <icanul@icanul.royalresorts.local>
2019-11-15 17:54:38 -0500

    changes in App

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,7 @@ import React,{Component} from 'react';
 class App extends Component{
     constructor(props){
         super(props);
-        this.state= {
-
-        }
+        this.state= {}
     }
     render(){
         return(
